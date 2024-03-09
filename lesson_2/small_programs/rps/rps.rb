bf1444a9652e8f085305ae51c0a7a84c9841137d
Nikko Macatangay <nikkomacatangay@gmail.com>
2024-03-09 12:34:21 +0000

    Implement corrections.

--- lesson_2/small_programs/rps/rps.rb
+++ lesson_2/small_programs/rps/rps.rb
@@ -2,17 +2,54 @@ require 'yaml'
 require 'pry'
 MESSAGES = YAML.load_file('rps_messages.yml')
 
+GAMES_TO_WIN = 3
+
+VALID_CHOICES = { 'r' => 'rock',
+                  'p' => 'paper',
+                  's' => 'scissors',
+                  'sp' => 'spock',
+                  'l' => 'lizard' }
+
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-def win?(player, computer)
+def get_name
+  name = ''
+  loop do
+    prompt("Enter your name:")
+    name = Kernel.gets().chomp()
+    break unless name.empty?
+    prompt(MESSAGES['valid_name'])
+  end
+  name
+end
+
+def player_choice?
+  prompt(MESSAGES['choices_prompt'])
+  prompt(MESSAGES['help'])
+  choice = ''
+  loop do
+    choice = Kernel.gets().chomp().downcase()
+    if VALID_CHOICES.key?(choice)
+      break
+    elsif choice == 'h'
+      prompt(MESSAGES['tutorial'])
+    else
+      prompt(MESSAGES['valid_choice'])
+    end
+  end
+  prompt("You chose: #{VALID_CHOICES[choice]}")
+  choice
+end
+
+def win?(winner, loser)
   rules = { 'r' => ['s', 'l'],
             'p' => ['r', 'sp'],
             's' => ['p', 'l'],
             'sp' => ['s', 'r'],
             'l' => ['p', 'sp'] }
-  rules[player].include?(computer)
+  rules[winner].include?(loser)
 end
 
 def display_results(player, computer)
@@ -33,50 +70,29 @@ def winner(player, computer)
   end
 end
 
+def grand_winner?
+  player_score = 0
+  computer_score = 0
+  until player_score == GAMES_TO_WIN || computer_score == GAMES_TO_WIN
+    prompt("#{NAME}: #{player_score} | Computer: #{computer_score}")
+    choice = player_choice?
+    computer_choice = VALID_CHOICES.keys.sample()
+    prompt("Computer cose: #{VALID_CHOICES[computer_choice]}")
+    display_results(choice, computer_choice)
+    player_score += 1 if win?(choice, computer_choice)
+    computer_score += 1 if win?(computer_choice, choice)
+    sleep(3)
+    system("clear")
+  end
+  prompt("#{NAME}: #{player_score} | Computer: #{computer_score}")
+  winner(player_score, computer_score)
+end
+
 def valid_input(input)
   input == 'y' || input == 'n'
 end
 
-valid_choices = { 'r' => 'rock',
-                  'p' => 'paper',
-                  's' => 'scissors',
-                  'sp' => 'spock',
-                  'l' => 'lizard' }
-
-prompt(MESSAGES['welcome'])
-
-name = ''
-loop do
-  prompt("Enter your name:")
-  name = Kernel.gets().chomp()
-  break unless name.empty?
-  prompt(MESSAGES['valid_name'])
-end
-
-loop do
-  player_score = 0
-  computer_score = 0
-  until player_score == 3 || computer_score == 3
-    prompt(MESSAGES['tutorial'])
-    choice = ''
-    loop do
-      prompt(MESSAGES['choices_prompt'])
-      choice = Kernel.gets().chomp().downcase()
-      break if valid_choices.key?(choice)
-      prompt(MESSAGES['valid_choice'])
-    end
-    computer_choice = valid_choices.keys.sample()
-    prompt("You chose: #{valid_choices[choice]}")
-    prompt("Computer cose: #{valid_choices[computer_choice]}")
-    display_results(choice, computer_choice)
-    if win?(choice, computer_choice)
-      player_score += 1
-    elsif win?(computer_choice, choice)
-      computer_score += 1
-    end
-    prompt("#{player_score} : #{computer_score}")
-    winner(player_score, computer_score)
-  end
+def play_again?
   user_input = ''
   loop do
     prompt(MESSAGES['play_again'])
@@ -84,7 +100,21 @@ loop do
     break if valid_input(user_input)
     prompt(MESSAGES['valid_choice'])
   end
-  break if user_input.include?('n')
+  user_input
 end
 
-prompt("Thank you for playing #{name}. Good bye!")
+prompt(MESSAGES['welcome'])
+
+prompt(MESSAGES['tutorial'])
+
+NAME = get_name
+
+loop do
+  system("clear")
+  grand_winner?
+  rematch = play_again?
+  system("clear")
+  break if rematch.include?('n')
+end
+
+prompt("Thank you for playing #{NAME}. Good bye!")
