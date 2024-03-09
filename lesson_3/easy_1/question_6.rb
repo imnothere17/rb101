87ed8c10d48e0eee237e918ca513c0a56bd0633b
Nikko Macatangay <nikkomacatangay@gmail.com>
2024-03-09 14:26:27 +0000

    Finished question 6.

---
+++ lesson_3/easy_1/question_6.rb
@@ -0,0 +1,9 @@
+famous_words = "seven years ago..."
+
+puts "Four score and #{famous_words}"
+
+puts "Four score and " + famous_words
+
+puts "Four score and " << famous_words
+
+puts famous_words.prepend("Four score and ")
