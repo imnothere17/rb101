c83a64351e050de72760716bb84e45d2b33a22bb
Nikko Macatangay <nikkomacatangay@gmail.com>
2024-03-10 10:24:43 +0000

    Finished Easy 2.

---
+++ lesson_3/easy_2/question_5.rb
@@ -0,0 +1,5 @@
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+
+flintstones_new = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+p flintstones, flintstones_new
---
+++ lesson_3/easy_2/question_8.rb
@@ -0,0 +1,5 @@
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+new_advice = advice.slice!(0, advice.index('house'))
+
+p advice, new_advice
